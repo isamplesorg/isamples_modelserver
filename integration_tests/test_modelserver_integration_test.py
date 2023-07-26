aa58d3ad5a558d43cc24b8e5f94466e70f293f36
Danny Mandel <danny.mandel@gmail.com>
2023-07-25 17:15:49 -0700

    Refactoring

--- integration_tests/test_modelserver_integration_test.py
+++ integration_tests/test_modelserver_integration_test.py
@@ -27,6 +27,11 @@ def headers():
     return {"accept": "application/json", "User-Agent": "iSamples Integration Bot 2000"}
 
 
+@pytest.fixture
+def modelserver_client(hostname: str, headers: dict):
+    return ModelServerClient(hostname, headers)
+
+
 class ModelServerClient:
     base_url: str
     base_headers: dict
@@ -35,34 +40,43 @@ class ModelServerClient:
         self.base_url = base_url
         self.base_headers = base_headers
 
-    def _make_json_request(self, url: str, rsession: requests.Session, data_params: dict) -> Any:
+    def _make_json_request(self, url: str, data_params: dict, rsession: requests.Session) -> Any:
         data_params_bytes: bytes = json.dumps(data_params).encode("utf-8")
         res = rsession.post(url, headers=self.base_headers, data=data_params_bytes)
         response_dict = res.json()
         return response_dict
 
+    def _make_opencontext_request(self, source_record: dict, type: str, rsession: requests.Session = requests.Session()) -> Any:
+        params: dict = {"source_record": source_record, "type": type}
+        url = f"{self.base_url}opencontext"
+        return self._make_json_request(url, params, rsession)
+
     def make_opencontext_material_request(self, source_record: dict, rsession: requests.Session = requests.Session()) -> Any:
-        url = f"{self.base_url}opencontext/"
-        params: dict = {"source_record": source_record, "type": "material"}
-        return self._make_json_request(url, rsession, params)
+        return self._make_opencontext_request(source_record, "material", rsession)
+
+    def make_opencontext_sample_request(self, source_record: dict, rsession: requests.Session = requests.Session()) -> Any:
+        return self._make_opencontext_request(source_record, "sample", rsession)
 
 
-def _post_opencontext_material(
-    rsession: requests.Session,
-    hostname: str,
-    headers: dict
-) -> list:
-    client = ModelServerClient(hostname, headers)
-    return client.make_opencontext_material_request({"foo": "bar"}, rsession)
-
-
-def test_opencontext_material(
-    rsession: requests.Session,
-    hostname: str,
-    headers: dict,
-):
-    response = _post_opencontext_material(rsession, hostname, headers)
+def _assert_opencontext_response(response):
     assert response is not None
     assert type(response) is list
     assert type(response[0]["value"]) is str
     assert type(response[0]["confidence"]) is float
+
+
+def test_opencontext_material(
+    rsession: requests.Session,
+    modelserver_client: ModelServerClient
+):
+    response = modelserver_client.make_opencontext_material_request({"foo": "bar"}, rsession)
+    _assert_opencontext_response(response)
+
+def test_opencontext_sample(
+    rsession: requests.Session,
+    modelserver_client: ModelServerClient
+):
+    response = modelserver_client.make_opencontext_sample_request({"foo": "bar"}, rsession)
+    _assert_opencontext_response(response)
+
+
